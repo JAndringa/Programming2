6d48d924d9a19025ce0dbd56647dac8d1cf15291
JAndringa <andringajibbe@gmail.com>
2023-02-22 13:41:21 +0100

    Week 3 starter kit

---
+++ Week3/linkedlist/main.cpp
@@ -0,0 +1,11 @@
+//==============================================================
+// Filename    : 
+// Authors     : 
+// Group       :
+// License     :  N.A. or opensource license like LGPL
+// Description : 
+//==============================================================
+
+int main() {
+    return 0;
+}
